66289d45d2e20150aeba53fef2c628d31ba00ec3
David <david.ferdinand@cashenable.com>
2026-02-26 20:27:15 +0700

    deploy baby 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/syhbd/', // GitHub Pages: https://<username>.github.io/syaibd/
+  base: '/syhbd/', // GitHub Pages: https://davidfim.github.io/syhbd/
 })
